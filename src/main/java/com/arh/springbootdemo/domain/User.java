035dff15265e0d3a16e70c4a05a79d495d6004d3
wudisorry <1192905560@qq.com>
2020-04-24 20:29:19 +0800

    whatever

---
+++ src/main/java/com/arh/springbootdemo/domain/User.java
@@ -0,0 +1,48 @@
+package com.arh.springbootdemo.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @Description
+ * @Author chenli
+ * @Date 2020/4/24
+ **/
+@Entity
+public class User implements Serializable {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @Column
+    private String name;
+    @Column
+    private Date birthday;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+}

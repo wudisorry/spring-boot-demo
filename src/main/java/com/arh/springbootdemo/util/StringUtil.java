73d5cb92acc5137b23de12e38aa886c37ceccb3c
wudisorry <1192905560@qq.com>
2020-05-11 10:15:31 +0800

    whatever

--- src/main/java/com/arh/springbootdemo/util/StringUtil.java
+++ src/main/java/com/arh/springbootdemo/util/StringUtil.java
@@ -2,6 +2,11 @@ package com.arh.springbootdemo.util;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @Description
  * @Author chenli
@@ -34,5 +39,17 @@ public class StringUtil extends StringUtils {
                 return new String(chs, 0, count);
             }
         }
+    }
+
+    public static List<String> getAtUser(String str){
+        Pattern p = Pattern.compile("(?<=@).*?(?= )");
+        Matcher m = p.matcher(str);
+        List<String> result=new ArrayList<>();
+        while(m.find()){
+            if(StringUtils.isNoneBlank(m.group().trim())){
+                result.add(m.group().trim());
+            }
+        }
+        return result;
     }
 }

ecb277aada32ceeb1404c83da1fe81862b7c9537
wudisorry <1192905560@qq.com>
2020-07-20 18:49:16 +0800

    whatever

---
+++ src/main/java/com/arh/springbootdemo/nio/netty/example/discard/DiscardServerHandler.java
@@ -0,0 +1,33 @@
+package com.arh.springbootdemo.nio.netty.example.discard;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+import java.nio.ByteBuffer;
+
+
+/**
+ * @Description
+ * @Author chenli
+ * @Date 2020/7/20
+ **/
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        try {
+            ByteBuf byteBuf = (ByteBuf) msg;
+            System.out.println(byteBuf.toString(io.netty.util.CharsetUtil.US_ASCII));
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
